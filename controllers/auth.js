0696fbc7867da945572749bd873c93ad78c04857
sanjay <ksanjayk@protonmail.com>
2022-06-06 00:15:42 +0530

    mongoose middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,23 +1,10 @@
 const { StatusCodes } = require("http-status-codes");
 const User = require("../models/User");
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs");
 
 const register = async (req, res) => {
-
-    const { name, email, password }= req.body
-
-    // salt is random bytes 
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password,salt)
-    
-    const tempUser ={
-        name,
-        email,
-        password:hashedPassword
-    }
-
-  const user = await User.create({ ...tempUser });
-  res.status(StatusCodes.CREATED).json({user});
+  const user = await User.create({ ...req.body });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
