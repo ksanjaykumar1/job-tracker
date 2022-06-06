61c147e0942e4840a8d87bafc643dda8de528be9
sanjay <ksanjayk@protonmail.com>
2022-06-06 23:34:16 +0530

    token generation in controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,16 @@
 const { StatusCodes } = require("http-status-codes");
 const User = require("../models/User");
-const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+
+  const token = jwt.sign(
+    { userId: user._id, name: user.name },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRY }
+  );
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
