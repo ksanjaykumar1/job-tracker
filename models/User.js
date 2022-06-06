5afe9a221a0480d7d2d48353baf86b90cf663854
sanjay <ksanjayk@protonmail.com>
2022-06-06 23:52:30 +0530

    json expiry env name update

--- models/User.js
+++ models/User.js
@@ -42,7 +42,7 @@ UserSchema.methods.createJWT = function () {
   return jwt.sign(
     { userId: this._id, name: this.name },
     process.env.JWT_SECRET,
-    { expiresIn: process.env.JWT_EXPIRY }
+    { expiresIn: process.env.JWT_LIFETIME }
   );
 };
 
