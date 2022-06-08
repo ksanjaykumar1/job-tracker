d931a4ee1cc72552cd8b88581c9386d212798a15
sanjay <ksanjayk@protonmail.com>
2022-06-08 18:51:44 +0530

    job routes protected

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 const connectDB = require("./db/connect");
 const Logger = require("./logger/logger");
@@ -11,29 +11,27 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 const notFound = require("./middleware/not-found");
 const logger = Logger.getLogger("./app");
 
-const jobs = require('./routes/jobs')
-const auth = require('./routes/auth')
+const jobs = require("./routes/jobs");
+const auth = require("./routes/auth");
+const authMiddlware = require("./middleware/auth");
 
 app.use(express.json({ extended: false }));
 
-app.use('/api/v1/auth',auth)
-app.use('/api/v1/jobs',jobs)
+app.use("/api/v1/auth", auth);
+app.use("/api/v1/jobs", authMiddlware, jobs);
 
-
-app.use(errorHandlerMiddleware)
-app.use(notFound)
-
-
+app.use(errorHandlerMiddleware);
+app.use(notFound);
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    logger.info('Connected to Mongo DB')
-    app.listen(PORT, ()=> logger.info(`Server started on ${PORT}..`))
+    logger.info("Connected to Mongo DB");
+    app.listen(PORT, () => logger.info(`Server started on ${PORT}..`));
   } catch (error) {
     logger.error("Failed to connect to Mongo DB", error);
     process.exit(1);
   }
 };
 
-start()
+start();
