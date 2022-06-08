6693b88756f3a3ee687435e9791e196aad24ffc3
sanjay <ksanjayk@protonmail.com>
2022-06-08 18:50:09 +0530

    authentication

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,10 @@
 const { BadRequestError, UnauthenticatedError } = require("../errors");
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
-
+const Logger = require("../logger/logger");
+const logger = Logger.getLogger("../middlware/auth");
 const auth = (req, res, next) => {
-  const authHeader = req.header.authorization;
+  const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith("Bearer")) {
     throw new UnauthenticatedError("Authenctication invalid ");
@@ -13,6 +14,10 @@ const auth = (req, res, next) => {
 
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
+    logger.info(JSON.stringify(payload));
+
+    // const user = User.findById(payload.userId).select('-password')
+    // req.user = user
 
     // attach user to job route
     req.user = { userId: payload.userId, name: payload.name };
