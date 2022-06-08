59f8c772e377517b3728934a7a77060722d1544b
sanjay <ksanjayk@protonmail.com>
2022-06-08 10:49:38 +0530

    authentication and login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 const User = require("../models/User");
-
+const Logger = require('../logger/logger')
+const logger = Logger.getLogger('./controllers/auth')
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
 
@@ -11,7 +13,27 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send("login user");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Provide username and password");
+  }
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new UnauthenticatedError("Invaild Credentials");
+  }
+
+  const isMatch = await user.comparePassword(password)
+  logger.info("Password match ==>",isMatch)
+  if (!isMatch) {
+    throw new UnauthenticatedError("Invalid Password");
+  }
+
+  // compare password
+
+  const token = user.createJWT();
+
+  res.status(StatusCodes.OK).json({ token, user: { name: user.name } });
 };
 
 module.exports = {
