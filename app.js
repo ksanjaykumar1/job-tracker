dfa4cb8d259fd2c993d9df9bb109c50c5ca41350
sanjay <ksanjayk@protonmail.com>
2022-06-09 20:20:05 +0530

    swagger setup

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 require("express-async-errors");
+
 const express = require("express");
 const app = express();
 
@@ -22,6 +23,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 app.use(express.json({ extended: false }));
 
 app.set("trust proxy", 1);
@@ -34,6 +40,11 @@ app.use(
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+app.get("/", (req, res) => {
+  res.send('<h1>Job Tracker API</h1> <a href="/api-docs"></a>');
+});
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/jobs", authMiddlware, jobs);
